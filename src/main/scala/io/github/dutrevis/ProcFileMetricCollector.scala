e6f64d6805c01fd1f95276677b3c1daa21713d4f
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2024-04-10 19:35:05 -0300

    fix: removed procFileName from getMetricValue

--- src/main/scala/io/github/dutrevis/ProcFileMetricCollector.scala
+++ src/main/scala/io/github/dutrevis/ProcFileMetricCollector.scala
@@ -5,10 +5,7 @@ import scala.io.Source
 trait ProcFileMetricCollector {
   protected val procFilePath: String
 
-  protected def getMetricValue(
-      procFileName: String,
-      originalMetricName: String
-  ): Any
+  protected def getMetricValue(originalMetricName: String): Any
 
   protected def getProcFileSource(): scala.io.Source = {
     Source.fromFile(procFilePath)
