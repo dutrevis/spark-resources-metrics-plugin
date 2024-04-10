220735ec601764ba527ea8a2ec6b3841fe8247f7
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2024-04-10 19:12:05 -0300

    fix: set procFileSource to use procFilePath value

--- src/main/scala/io/github/dutrevis/ProcFileMetricCollector.scala
+++ src/main/scala/io/github/dutrevis/ProcFileMetricCollector.scala
@@ -3,14 +3,14 @@ package io.github.dutrevis
 import scala.io.Source
 
 trait ProcFileMetricCollector {
-  protected val procFileName: String
+  protected val procFilePath: String
 
   protected def getMetricValue(
       procFileName: String,
       originalMetricName: String
   ): Any
 
-  protected def procFileSource(procFilePath: String) {
+  protected def procFileSource(): scala.io.Source = {
     Source.fromFile(procFilePath)
   }
 
