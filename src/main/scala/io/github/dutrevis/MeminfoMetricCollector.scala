83ba764c18a49a4e3fc29c843dc34849e7495dfc
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2024-05-07 14:01:06 -0300

    feat: removed io.Source usage

--- src/main/scala/io/github/dutrevis/MeminfoMetricCollector.scala
+++ src/main/scala/io/github/dutrevis/MeminfoMetricCollector.scala
@@ -1,18 +1,14 @@
 package io.github.dutrevis
 
-import scala.io.{Source, BufferedSource}
-
-class MeminfoMetricCollector(
-    sourceMethod: String => BufferedSource = Source.fromFile
-) extends ProcFileMetricCollector {
+class MeminfoMetricCollector extends ProcFileMetricCollector {
 
   override val procFilePath = "/proc/meminfo"
 
-  /** Gets the value of a metric from a proc file located at the `procFilePath`
-    * value set. The metric is searched according to the original name provided
-    * in the parameter `originalMetricName`. <p>
-    * @param procFileSource
-    *   a BufferedSource instance, with access to the desired proc file <p>
+  /** Gets the value of a metric from the content of a proc file provided. The
+    * metric is searched according to the original name provided in the
+    * parameter `originalMetricName`. <p>
+    * @param procFileContent
+    *   a String with the content of the desired proc file <p>
     * @param originalMetricName
     *   the original metric name by which it is found in the proc file <p>
     * @return
@@ -21,24 +17,16 @@ class MeminfoMetricCollector(
     *   if a metric is not found with the provided original name
     */
   override def getMetricValue(
-      procFileSource: BufferedSource,
+      procFileContent: String,
       originalMetricName: String
   ): Long = {
-    val procFileData = procFileSource.getLines
-      .filter(metricLine => metricLine.contains(originalMetricName))
+    val procFileData = procFileContent.linesIterator
+      .filter(metricLine => metricLine.toString.contains(originalMetricName))
       .map { case s: String => s.split(":") }
       .map { case Array(k, v) => k -> v.trim().split(" ").head.toLong }
       .toMap
     val metricValue = procFileData(originalMetricName)
-    procFileSource.close()
     metricValue
   }
 
-  /** Access and buffers a proc file located at the `procFilePath`. <p>
-    * @return
-    *   a `BufferedSource` instance of the file read
-    */
-  override def getProcFileSource(): scala.io.BufferedSource = {
-    sourceMethod(procFilePath)
-  }
 }
